9f824679de4f2a6cac08f7c246f2b46023ac974d
danixuz <danixuz@icloud.com>
2022-05-15 16:42:50 +0300

    Initial Commit

---
+++ AppStoreAnimation/AppStoreAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreAnimationApp.swift
+//  AppStoreAnimation
+//
+//  Created by Daniel Spalek on 15/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
